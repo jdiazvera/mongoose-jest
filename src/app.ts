a9f66c2cd549d5f4b68df1daaf4c211f9b321fe3
julio <jdiazvera@gmail.com>
2022-07-29 17:04:29 -0400

    probando con app.listen.close

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
 import path from "path";
 
 import { loadApiEndpoints } from "./controllers/api";
 
 // Create Express server
 const app = express();
+
+// set up mongoose
+mongoose
+  .connect(
+    "mongodb+srv://mongo:123...%40bcD.@cluster0.nsrarlo.mongodb.net/myDele"
+  )
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch(() => {
+    console.log("Error connecting to database");
+  });
 
 // Express configuration
 app.set("port", process.env.PORT || 3000);
--- test/api.spec.ts
+++ test/api.spec.ts
@@ -3,6 +3,9 @@ import request from "supertest";
 import app from "../src/app";
 
 describe("GET /api", () => {
+  afterAll(() => {
+    app.listen(3000).close();
+  });
   it("should return 200 OK", () => {
     return request(app).get("/api").expect(200);
   });
